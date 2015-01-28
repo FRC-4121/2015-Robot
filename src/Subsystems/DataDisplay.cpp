9dc49b946e40c9f5ab4b503eab58df0d63c35c49
TheGheff <armstrong.jeffrey@gmail.com>
2015-01-27 20:29:19 -0500

    SmartDashboard Gyro
    We are adding the angle output from the gyro. Is not done but builds.
    Subsystem has been created but command and default command need to still
    be created

---
+++ src/Subsystems/DataDisplay.cpp
@@ -0,0 +1,26 @@
+#include "DataDisplay.h"
+#include "../RobotMap.h"
+#include "OI.h"
+#include "../CommandBase.h"
+DataDisplay::DataDisplay() :
+		Subsystem("ExampleSubsystem")
+{
+
+}
+
+void DataDisplay::InitDefaultCommand()
+{
+
+		// Set the default command for a subsystem here.
+//	SetDefaultCommand(new MySpecialCommand());
+}
+
+// Put methods for controlling this subsystem
+// here. Call these from Commands.
+
+void DisplayAll()
+{
+
+	SmartDashboard::PutNumber("Gyro: ", CommandBase::oi->getGyro()->GetAngle());
+
+}
